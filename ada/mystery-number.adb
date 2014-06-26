ec0f90f5dccba2dcc3070b7c48489594c65c5534
Jérémy Moreau <jmoreau@teclib.com>
2014-06-26 14:44:49 +0200

    Remove ADA build files

---
+++ ada/mystery-number.adb
@@ -0,0 +1,10 @@
+WITH ada.text_io;    USE ada.text_io;
+
+PROCEDURE mystery_number IS
+
+BEGIN
+   Put_Line("/******************************************************************************\");
+   Put_Line("|                                  WELCOME TO                                  |");
+   Put_Line("|                                MYSTERY NUMBER                                |");
+   Put_Line("\******************************************************************************/");
+END mystery_number;
