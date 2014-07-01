b510628cbf51a52f06a549f3d65e4b4caf6658d5
Jérémy MOREAU <jmoreau@teclib.com>
2014-07-01 14:27:38 +0200

    Fix several minor bugs

--- php/mystery-number.php
+++ php/mystery-number.php
@@ -4,6 +4,8 @@
 if(substr(PHP_OS, 0, 3) == "WIN") define('CLS', 'cls');
 else  define('CLS', 'clear');
 
+mb_internal_encoding("utf-8");
+
 define('SOLO', 1);
 define('MULTI', 2);
 
@@ -87,7 +89,7 @@ if(4 == $difficulty) {
       system(CLS);
       echo $lang[7];                                        // Please, enter the maximum number
       $input = fgets(STDIN);
-   } while((int) $input != $input || $input < 1 || $input > 4);
+   } while((int) $input != $input);
 
    $max = (int) $input;
 
@@ -124,15 +126,18 @@ while($continue) {
 
          // You must enter a smaller number ! This one is greater than the maximum difficulty
          if((int) $input > $max) {
-            echo $lang[20];
+            echo trim($lang[20]) . '('.$max.')';
             sleep(3);
          }
       } while((int) $input != $input || $input < 1 || (int) $input > $max);
 
       $random = (int) $input;
    } else {
-      $random = mt_rand(0, $max);
+      $random      = mt_rand(0, $max);
    }
+
+   $player_name   = trim($player_name);
+   $opposant_name = trim($opposant_name);
 
    system(CLS);
    if(SOLO == $mode) {
@@ -158,6 +163,7 @@ while($continue) {
    $spend_time    = time() - $start_time;
    system(CLS);
    echo sprintf($lang[13], $random, $attempt_count, floor($spend_time / 60), $spend_time % 60) . PHP_EOL;
+   sleep(3);
 
    // Score calculation
    $score = 2 * $max - $attempt_count - $spend_time;
@@ -170,45 +176,63 @@ while($continue) {
 
    // 7. Scores
    if(SOLO == $mode || 2 == $turn) {
+      // Prepare Score tab strings
+      $title    = mb_str_pad(trim($lang[22], PHP_EOL), 76, ' ', STR_PAD_BOTH);
+      $names    = mb_str_pad(trim($lang[23], PHP_EOL), 17, ' ', STR_PAD_BOTH);
+      $attempts = mb_str_pad(trim($lang[24], PHP_EOL), 10, ' ', STR_PAD_BOTH);
+      $time     = mb_str_pad(trim($lang[25], PHP_EOL), 12, ' ', STR_PAD_BOTH);
+      $score    = mb_str_pad(trim($lang[26], PHP_EOL), 15, ' ', STR_PAD_BOTH);
+      $date     = mb_str_pad(trim($lang[27], PHP_EOL), 10, ' ', STR_PAD_BOTH);
+
       if(MULTI == $mode) {
-         // Player 1 : attemps = x; spend time = m min s s ; score = Score 1
-         echo sprintf($lang[18],
-                      trim(ucfirst($player[1]['name']), PHP_EOL),
-                      $player[1]['attemps'],
-                      floor($player[1]['spend_time'] / 60),
-                      $player[1]['spend_time'] % 60,
-                      $player[1]['score']);
-         // Player 2 : attemps = x; spend time = m min s s ; score = Score 2
-         echo sprintf($lang[18],
-                      trim(ucfirst($player[2]['name']), PHP_EOL),
-                      $player[2]['attemps'],
-                      floor($player[2]['spend_time'] / 60),
-                      $player[2]['spend_time'] % 60,
-                      $player[2]['score']);
 
-         if($player[1]['score'] > $player[2]['score']) {
-            echo sprintf($lang[19], trim(ucfirst($player[1]['name']), PHP_EOL)) . PHP_EOL;      // Player 1 win
-         } elseif($player[2]['score'] > $player[1]['score']) {
-            echo sprintf($lang[19], trim(ucfirst($player[2]['name']), PHP_EOL)) . PHP_EOL;      // Player 2 win
+         echo PHP_EOL . PHP_EOL;
+         echo "/******************************************************************************\\" . PHP_EOL;
+         echo "| $title |" . PHP_EOL;
+         echo "|------------------------------------------------------------------------------|" . PHP_EOL;
+         echo "| $names | $attempts | $time | $score | $date |" . PHP_EOL;
+         echo "|------------------------------------------------------------------------------|" . PHP_EOL;
+
+         // Player 1
+         echo '| ' . mb_str_pad(trim(ucfirst($player[1]['name'])), 17, ' ', STR_PAD_RIGHT)
+              . ' | ' . mb_str_pad($player[1]['attemps'], 10, ' ', STR_PAD_BOTH)
+              . ' | ' . mb_str_pad(floor($player[1]['spend_time'] / 60) . ' min '
+                      . $player[1]['spend_time'] % 60 . ' s', 12, ' ', STR_PAD_BOTH)
+              . ' | ' . mb_str_pad($player[1]['score'], 15, ' ', STR_PAD_BOTH)
+              . ' | ' . mb_str_pad(date('Y-m-d'), 10, ' ', STR_PAD_BOTH) . ' |' . PHP_EOL;
+         // Player 2
+         echo '| ' . mb_str_pad(trim(ucfirst($player[2]['name'])), 17, ' ', STR_PAD_RIGHT)
+              . ' | ' . mb_str_pad($player[2]['attemps'], 10, ' ', STR_PAD_BOTH)
+              . ' | ' . mb_str_pad(floor($player[2]['spend_time'] / 60) . ' min '
+                      . $player[2]['spend_time'] % 60 . ' s', 12, ' ', STR_PAD_BOTH)
+              . ' | ' . mb_str_pad($player[2]['score'], 15, ' ', STR_PAD_BOTH)
+              . ' | ' . mb_str_pad(date('Y-m-d'), 10, ' ', STR_PAD_BOTH) . ' |' . PHP_EOL;
+
+         echo "|------------------------------------------------------------------------------|" . PHP_EOL;
+         if($player[1]['score'] == $player[2]['score']) {
+            echo '| ' . mb_str_pad(trim($lang[21]), 76, ' ', STR_PAD_BOTH) . ' |' . PHP_EOL;
          } else {
-            echo $lang[21] . PHP_EOL;                                                           // Match null
+            $winner = ($player[1]['score'] > $player[2]['score']) ? $player[1]['name'] : $player[2]['name'];
+            $winner = mb_strtoupper(trim(sprintf($lang[19], trim($winner, PHP_EOL), PHP_EOL)));
+            echo '| ' . mb_str_pad($winner, 76, ' ', STR_PAD_BOTH) . ' |' . PHP_EOL;
          }
-         $winner = ($player[1]['score'] > $player[2]['score']) ? $player[1]['name'] : $player[2]['name'];
-      }
+         echo "\******************************************************************************/" . PHP_EOL;
+         echo PHP_EOL . PHP_EOL;
 
-      // Get previous best scores
-      if(is_file('scores.txt')) {
-         $fp = fopen('scores.txt', 'r');
-         while($line = fgets($fp)) {
-            $best_scores[] = explode('|', trim($line, PHP_EOL));
-         }
-         fclose($fp);
       } else {
+         // Get previous best scores
          $best_scores = array();
-      }
+         if(is_file('scores.txt')) {
+            $fp = fopen('scores.txt', 'r');
+            while($line = fgets($fp)) {
+               $best_scores[] = explode('|', trim($line, PHP_EOL));
+            }
+            fclose($fp);
+         } else {
+            $best_scores = array();
+         }
 
-      // Add pplayer score if it's a solo game
-      if(SOLO == $mode) {
+         // Add player score
          $best_scores[] = array(
             $player_name,
             $attempt_count,
@@ -216,52 +240,43 @@ while($continue) {
             $score,
             date('Y-m-d')
          );
-      }
 
-      // Order Array
-      for($i = 0; $i < sizeof($best_scores) - 1 ; $i++) {
-         for($j = $i+1; $j < sizeof($best_scores) ; $j++) {
-            if($best_scores[$j][3] > $best_scores[$i][3]) {
-               $tmp_tab = $best_scores[$i];
-               $best_scores[$i] = $best_scores[$j];
-               $best_scores[$j] = $tmp_tab;
+         // Order Array
+         for($i = 0; $i < sizeof($best_scores) - 1 ; $i++) {
+            for($j = $i+1; $j < sizeof($best_scores) ; $j++) {
+               if($best_scores[$j][3] > $best_scores[$i][3]) {
+                  $tmp_tab = $best_scores[$i];
+                  $best_scores[$i] = $best_scores[$j];
+                  $best_scores[$j] = $tmp_tab;
+               }
             }
          }
-      }
 
-      // Delete the last line if more than 10 lines
-      if(sizeof($best_scores) > 10) {
-         array_pop($best_scores);
-      }
+         // Delete the last line if more than 10 lines
+         if(sizeof($best_scores) > 10) {
+            array_pop($best_scores);
+         }
 
-      // Show best scores
-      $title    = str_pad(trim($lang[22], PHP_EOL), 76, ' ', STR_PAD_BOTH);
-      $names    = str_pad(trim($lang[23], PHP_EOL), 17, ' ', STR_PAD_BOTH);
-      $attempts = str_pad(trim($lang[24], PHP_EOL), 10, ' ', STR_PAD_BOTH);
-      $time     = str_pad(trim($lang[25], PHP_EOL), 10, ' ', STR_PAD_BOTH);
-      $score    = str_pad(trim($lang[26], PHP_EOL), 10, ' ', STR_PAD_BOTH);
-      $date     = str_pad(trim($lang[27], PHP_EOL), 17, ' ', STR_PAD_BOTH);
+         // Show best scores
+         echo PHP_EOL . PHP_EOL;
+         echo "/******************************************************************************\\" . PHP_EOL;
+         echo "| $title |" . PHP_EOL;
+         echo "|------------------------------------------------------------------------------|" . PHP_EOL;
+         echo "| $names | $attempts | $time | $score | $date |" . PHP_EOL;
+         echo "|------------------------------------------------------------------------------|" . PHP_EOL;
+         foreach($best_scores as $player_datas) {
+            $player_name     = mb_str_pad($player_datas[0], 17, ' ', STR_PAD_RIGHT);
+            $player_attempts = mb_str_pad($player_datas[1], 10, ' ', STR_PAD_BOTH);
+            $player_time     = mb_str_pad($player_datas[2], 12, ' ', STR_PAD_BOTH);
+            $player_score    = mb_str_pad($player_datas[3], 15, ' ', STR_PAD_BOTH);
+            $player_date     = mb_str_pad($player_datas[4], 10, ' ', STR_PAD_BOTH);
+            echo "| $player_name | $player_attempts | $player_time | $player_score | $player_date |" . PHP_EOL;
+         }
 
-      echo PHP_EOL . PHP_EOL;
-      echo "/******************************************************************************\\" . PHP_EOL;
-      echo "| $title |" . PHP_EOL;
-      echo "|------------------------------------------------------------------------------|" . PHP_EOL;
-      echo "| $names | $attempts | $time | $score | $date |" . PHP_EOL;
-      echo "|------------------------------------------------------------------------------|" . PHP_EOL;
-      foreach($best_scores as $player_datas) {
-         $player_name     = str_pad($player_datas[0], 17, ' ', STR_PAD_RIGHT);
-         $player_attempts = str_pad($player_datas[1], 10, ' ', STR_PAD_BOTH);
-         $player_time     = str_pad($player_datas[2], 10, ' ', STR_PAD_BOTH);
-         $player_score    = str_pad($player_datas[3], 10, ' ', STR_PAD_BOTH);
-         $player_date     = str_pad($player_datas[4], 17, ' ', STR_PAD_BOTH);
-         echo "| $player_name | $player_attempts | $player_time | $player_score | $player_date |" . PHP_EOL;
-      }
+         echo "\******************************************************************************/" . PHP_EOL;
+         echo PHP_EOL . PHP_EOL;
 
-      echo "\******************************************************************************/" . PHP_EOL;
-      echo PHP_EOL . PHP_EOL;
-
-      // Save best scores if it's a solo game
-      if(SOLO == $mode) {
+         // Save best scores
          $fp = fopen('scores.txt', 'w');
          foreach($best_scores as $player_datas) {
             fputs($fp, implode('|', $player_datas) . PHP_EOL);
@@ -277,4 +292,8 @@ while($continue) {
       }
       system(CLS);
    }
+}
+
+function mb_str_pad($input, $pad_length, $pad_string, $pad_style, $encoding = "UTF-8") {
+   return str_pad($input, strlen($input) - mb_strlen($input, $encoding) + $pad_length, $pad_string, $pad_style);
 }
