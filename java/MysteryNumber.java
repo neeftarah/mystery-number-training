7ae60568dcd9c56104b53c14ab5250c6b7d0f064
Jérémy MOREAU <jmoreau.dev@gmail.com>
2014-07-24 15:58:05 +0200

    Init java

---
+++ java/MysteryNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MysteryNumber
+{
+   public static void main(String[] args)
+   {
+
+      // 1. Show splash sreen for 3s
+      system(CLS);
+      System.out.println("/******************************************************************************\\");
+      System.out.println("|                                  WELCOME TO                                  |");
+      System.out.println("|                                MYSTERY NUMBER                                |");
+      System.out.println("\******************************************************************************/");
+
+
+   }
+}
